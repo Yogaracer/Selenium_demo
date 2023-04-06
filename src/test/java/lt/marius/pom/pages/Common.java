0ed0763348e90f0e39ca125abd839102f682f086
Yogaracer <talalasmarius@gmail.com>
2023-04-06 18:51:40 +0300

    Add Common class

---
+++ src/test/java/lt/marius/pom/pages/Common.java
@@ -0,0 +1,51 @@
+package lt.marius.pom.pages;
+
+import lt.marius.pom.utils.Driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+
+public class Common {
+
+    public static void setUpDriver() {
+        Driver.setDriver();
+    }
+
+    public static void openUrl(String url) {
+        Driver.getDriver().get(url);
+    }
+
+    public static void closeDriver() {
+        Driver.closeDriver();
+    }
+
+    public static void sleep(int millis){
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            Thread.currentThread().interrupt();
+        }
+    }
+
+    private static WebElement getElement(By locator) {
+        return Driver.getDriver().findElement(locator);
+    }
+
+    private static List<WebElement> getElements(By locator){
+        return Driver.getDriver().findElements(locator);
+    }
+
+    public static void sendKeysToElement(By locator, String sendKeys) {
+        getElement(locator).sendKeys(sendKeys);
+    }
+
+    public static void clickOnElement(By locator) {
+        getElement(locator).click();
+    }
+
+    public static String getTextFromElement(By locator) {
+        return getElement(locator).getText();
+    }
+}
