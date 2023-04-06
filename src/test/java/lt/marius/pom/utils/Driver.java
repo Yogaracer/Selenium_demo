65b9cdb02a85e687b7776ef4af72706ea28d3cb4
Yogaracer <talalasmarius@gmail.com>
2023-04-06 18:41:36 +0300

    Add Driver class

---
+++ src/test/java/lt/marius/pom/utils/Driver.java
@@ -0,0 +1,33 @@
+package lt.marius.pom.utils;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.time.Duration;
+
+public class Driver {
+
+    private static WebDriver driver;
+
+    public static void setDriver() {
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        options.addArguments("start-maximized");
+        options.addArguments("--force-device-scale-factor=0.70");
+
+        driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(8));
+    }
+
+    public static WebDriver getDriver() {
+        return driver;
+    }
+
+    public static void closeDriver() {
+        driver.quit();
+    }
+
+}
