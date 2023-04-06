ecd252c0cd0cf64531ec67868daf4f9324c4778c
Yogaracer <talalasmarius@gmail.com>
2023-04-06 18:53:53 +0300

    Add TestBase class

---
+++ src/test/java/lt/marius/pom/tests/TestBase.java
@@ -0,0 +1,17 @@
+package lt.marius.pom.tests;
+
+import lt.marius.pom.pages.Common;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public abstract class TestBase {
+
+    @BeforeMethod
+    public abstract void setUp();
+
+    @AfterMethod
+    public void tearDown() {
+        Common.closeDriver();
+    }
+
+}
